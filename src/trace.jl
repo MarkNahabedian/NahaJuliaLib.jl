1432e6fc685566be596c710ff24f7d97e49c9cb1
MarkNahabedian <naha@mit.edu>
2022-04-23 11:02:26 -0400

    Checkpoint @trace.  Almost works.  How to output the values of the arguments instead of their names though?

--- src/trace.jl
+++ src/trace.jl
@@ -28,14 +28,51 @@ end
 
 
 """
+    functionSignature(pieces)::Expr
+Return an expression represesenting the signature (function name and
+formal parameters) of a function definition.  `pieces` is the result
+of calling MacroTools.splitdef on the function definition.
+"""
+function functionSignature(pieces)::Expr
+    function rtype()
+        if haskey(pieces, :rtype)
+            Expr(:(::),
+                 call(),
+                 pieces[:rtype])
+        else
+            call()
+        end
+    end
+    function call()
+        if length(pieces[:kwargs]) > 0
+            Expr(:foo)  # TBD
+        else
+            Expr(:call, pieces[:name],
+                 pieces[:args]...)
+        end
+    end
+    rtype()
+end
+
+# We probably cant define an external function that will give the
+# function call expression because we don't have a way to get the
+# argument values.
+
+
+"""
     @trace(global_flag, definition)
 Cause the call arguments and return values of the definition to be
 logged if `global_flag` is true at run time.
 `definition` should define a method.
 """
 macro trace(global_flag, definition)
+    definition = longdef(definition)
     pieces = splitdef(definition)
     result = gensym("result")
+    failed = gensym("fAIlEd")
+    bodyfunction = gensym("bodyfunction")
+    arg = gensym("arg")
+    call = gensym("call")
     function gsarg(arg)
         gs = find_name(arg)
         (gs, arg)   # replace_name(arg, gs))
@@ -44,24 +81,69 @@ macro trace(global_flag, definition)
     kwargs = map(gsarg, pieces[:kwargs])
     # We must suppress hygiene because the function being traced could
     # refer to variables defined in an outer scope.
-    esc(
-        quote
+    Expr(:escape,
+         Expr(definition.head,
+              definition.args[1],   # function signature
+              Expr(:block,
+                   Expr(:function,
+                        Expr(:call, bodyfunction),
+                        pieces[:body]),
+                   Expr(:if, global_flag,
+                        #=
+                        # ERROR: LoadError: LoadError: MethodError: no method matching logmsg_code(::Module, ::String, ::Int64, ::Symbol)
+                        # Closest candidates are:
+                        # logmsg_code(::Any, ::Any, ::Any, ::Any, !Matched::Any, !Matched::Any...) at logging.jl:303
+                        Expr(:macrocall, :(@info), "Trace Enter"
+                             ) =#
+                        # This gives
+                        # (println)("Trace Enter ", (string)($(Expr(:copyast, :($(QuoteNode(:(hanoi(from, to, other, count)))))))))
+                        # but we want the values of the arguments, not their names.
+                        Expr(:call, println,
+                             "Trace Enter ",
+                             Expr(:call, string,
+                                  Expr(:quote,
+                                       Expr(:call, pieces[:name],
+                                            map(find_name, pieces[:args])...))))
+                        ),
+
+                   Expr(:(=), result, Expr(:call, bodyfunction)),
+                   Expr(:if, global_flag,
+                        #=
+                        Expr(:macrocall, :(@info), "Trace Exit"
+                             ) =#
+                        Expr(:if, false, # Expr(:call, :(==), result, Expr(:quote, failed)),
+                             Expr(:call, println, "Trace abnornal exit"),   # this can hapen if body explicitly returns.
+                             Expr(:call, println, "Trace Exit ", result))
+                        )),
+              result))
+#=
+        esc(quote
             function $(pieces[:name])(
                 $(map(last, args)...) ;
                 $(map(last, kwargs)...))
                 if $global_flag
+                    $call = if $(length(kwargs) > 0)
+                        Expr(:call, pieces[:name],
+                             Expr(:parameters,
+                                  (map(map(first, kwargs)) do $arg
+                                       Expr(:kw, :($arg), $arg)
+                                   end)...,
+                                  (map(first, args))))
+                    else
+                        Expr(:call, pieces[:name],
+                             map(first, args)...)
+                    end
+                    @info("Trace Enter", call=$call)
+                    #=
                     @info("Trace Enter",
-                          Expr(:call, $(pieces[:name]),
-                               Expr(:parameters,
-                                    Expr(:kw,
-                                         $(map(first, kwargs)...)),
-                                    $(map(first, args)...)))
-                          #=
-                          :($$(pieces[:name])(
-                              $$(map(first, args)...);
-                              $$(map(first, kwargs)...)))
-                          =#
-                          )
+                          call = $(string(
+                              Expr(:call, (pieces[:name]),
+                                   Expr(:parameters,
+                                        (map(map(first, kwargs)) do $arg
+                                            Expr(:kw, :($arg), $arg)
+                                        end)...,
+                                        (map(first, args))...)))))
+                    =#
                 end
                 $result = nothing
                 try
@@ -73,6 +155,6 @@ macro trace(global_flag, definition)
                 end
                 $result
             end
-        end)
+        end =#
 end
 
