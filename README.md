be095c72756688b7b1b29361d019a1539b02522d
MarkNahabedian <naha@mit.edu>
2022-05-03 14:02:23 -0400

    Set up for GitHub CI testing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NahaJuliaLib
+
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://MarkNahabedian.github.io/NahaJuliaLib.jl/stable)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://MarkNahabedian.github.io/NahaJuliaLib.jl/dev)
+[![Build Status](https://github.com/MarkNahabedian/NahaJuliaLib.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/MarkNahabedian/NahaJuliaLib.jl/actions/workflows/CI.yml?query=branch%3Amaster)
+
+
+A collection of small utilities that I wish were readily available in
+Julia but that I've not found elsewhere.
+
+See the documentation.
--- docs/make.jl
+++ docs/make.jl
@@ -4,3 +4,25 @@ using Pkg
 Pkg.add(url="../../NahaJuliaLib")
 
 makedocs(sitename="localhost")
+
+DocMeta.setdocmeta!(NahaJuliaLib, :DocTestSetup, :(using NahaJuliaLib); recursive=true)
+
+makedocs(;
+    modules=[NahaJuliaLib],
+    authors="MarkNahabedian <naha@mit.edu> and contributors",
+    repo="https://github.com/MarkNahabedian/NahaJuliaLib.jl/blob/{commit}{path}#{line}",
+    sitename="NahaJuliaLib.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://MarkNahabedian.github.io/NahaJuliaLib.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/MarkNahabedian/NahaJuliaLib.jl",
+    devbranch="master",
+)
