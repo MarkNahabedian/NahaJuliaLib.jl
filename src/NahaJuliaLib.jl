8d8a8d504b3587cef032983795cc34ac6be93db7
Mark Nahabedian <33787283+MarkNahabedian@users.noreply.github.com>
2023-01-31 05:49:12 -0500

    Don't load a file that doesn't work yet.

--- src/NahaJuliaLib.jl
+++ src/NahaJuliaLib.jl
@@ -5,7 +5,7 @@ include("uri_utils.jl")
 include("properties.jl")
 include("trace.jl")
 include("trace_analysis.jl")
-include("export_subtypes.jl")
+# include("export_subtypes.jl")
 include("package_utils.jl")
 include("my_package_template.jl")
 
